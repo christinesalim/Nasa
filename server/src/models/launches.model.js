5be21b18f6c67a4f62f35fcca13db087b27ffcec
Christine Salim <christinesalim@gmail.com>
2022-11-09 10:06:30 -0800

    clean up launches file

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,34 +7,18 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_LAUNCH_NUMBER = 100;
 
-/* Test data to use before MongoDB Launch collection was setup
-const launches = new Map();
-
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: 'Kepler Exploration X', //name
-  rocket: 'Explorer IS1', //found under rocket.name
-  launchDate: new Date('February 7, 2030'), //date_local
-  target: 'Kepler-442 b', //not applicable
-  customers: ['NASA', 'U.S. Govt'],//payload.customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-*/
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function findLaunch(filter) {
-  return await launchesDatabase.findOne(filter);
-}
 
-//get launches from SpaceX api
+// Get launches from SpaceX api
+// The populate field gets the referenced data from the rocket and payloads paths
+// and the fields from those paths
 async function populateLaunches() {
-  console.log('Downloading launch data...');
+  //console.log('Downloading launch data...');
   const response = await axios.post( SPACEX_API_URL, {
     query: {},
     options: {
-      pagination: false,
+      pagination: false, //gets all launches in one request from API
       populate: [
         {
           path: 'rocket',
@@ -60,8 +44,11 @@ async function populateLaunches() {
   const launchDocs = response.data.docs;
   for(const launchDoc of launchDocs){
     const payloads = launchDoc['payloads'];
+
+    //Process an array of payloads
     const customers = payloads.flatMap((payload) => {
       //mongoose populates the payload.customers data
+      //consolidate multiple customers into one array of customers
       return payload['customers'];
     })
 
@@ -75,13 +62,17 @@ async function populateLaunches() {
       customers,
     };
 
-    console.log(`${launch.flightNumber} ${launch.mission} ${customers}`);
-
     //Populate launches collection in database...
     await saveLaunch(launch);
   }
 }
 
+// Find a launch given the filter settings
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter);
+}
+
+// Load launch data from the external SpaceX API
 async function loadLaunchData(){
   const firstLaunch = await findLaunch({
     flightNumber: 1,
@@ -99,6 +90,8 @@ async function loadLaunchData(){
 
 //Returns a JS array with launch data, 
 //Remove document id and __v mongoose version key columns
+//skip: how many results to skip in the database
+//limit: the number of documents to return
 async function getAllLaunches(skip, limit){
   return await launchesDatabase
   .find({}, {'id': 0, '__v': 0 })
@@ -107,20 +100,11 @@ async function getAllLaunches(skip, limit){
   .limit(limit);
 }
 
-//Save launch by inserting it if it's new otherwise update
-//the existing launch if the flightNumber already exists in the 
-//database.
+//Save launch by inserting it into database
+//Some launches won't have a target planet such as the SpaceX launches
+//Launches scheduled in the client will have a target set and that information
+//is validated in the scheduleNewLaunch() function
 async function saveLaunch(launch){   
-
-  //Throw an error if we try to schedule a launch to an invalid planet
-  //SpaceX launches are to other exoplanets
-  /*const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-  if (!planet){
-    console.log(`${launch.target} No matching planet found`);
-  }*/
-
   await launchesDatabase.findOneAndUpdate({
     flightNumber: launch.flightNumber,
   }, launch, {
@@ -129,7 +113,10 @@ async function saveLaunch(launch){
 }
 
 
-//Schedule a new launch 
+// Schedule a new launch. Used when the client tries to schedule a new launch.
+// The target planet for the launch is validated before saving the launch.
+// For now the launch customers are hard-coded; we could add this to the client user
+// interface in the future.
 async function scheduleNewLaunch(launch){
   const planet = await planets.findOne({
     keplerName: launch.target,
@@ -146,7 +133,7 @@ async function scheduleNewLaunch(launch){
   const newLaunch = Object.assign(launch, {
     success: true,
     upcoming: true,
-    customers: ['U.S.A.', 'NASA'], //customers are hard-coded for now
+    customers: ['U.S.A.', 'NASA'],
     flightNumber: newFlightNumber,
   });
   try {
@@ -175,11 +162,14 @@ async function getLatestFlightNumber(){
   return latestLaunch.flightNumber;
 }
 
+//Cancel a mission by using the id to find it in the database and mark it
+//as not upcoming and not successful
 async function abortLaunchById(launchId){
   //Update the launch to indicate it was aborted
   const aborted = await launchesDatabase.updateOne({
     flightNumber: launchId,
   }, {
+    //Changing the upcoming status to false aborts the launch
     upcoming: false,
     success:false,
   });
